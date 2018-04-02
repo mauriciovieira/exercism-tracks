d4f0dd2a543f8a6b1e1627115bec702a64692daf
Mauricio Vieira <mauricio@smartly.io>
2018-04-02 21:25:28 +0300

    [:gem:] hamming

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,12 @@
+module BookKeeping
+  VERSION = 3
+end
+
+class Hamming
+  def self.compute(a, b)
+    a_arr = a.split('')
+    b_arr = b.split('')
+    raise ArgumentError if a.size != b.size
+    a_arr.zip(b_arr).count{|pair| pair[0] != pair[1]}
+  end
+end
