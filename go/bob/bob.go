7055cc92439ba8147417df43957e6558f30d500b
Mauricio Vieira <mauricio@smartly.io>
2018-04-15 21:16:24 +0300

    [go] Use strings and struct instead of regexp

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,33 +1,41 @@
-// Package bob implements a simple chatbot
+// Package bob exports the Hey function that will respond to a string as a snarky teenager would
 package bob
 
-import "regexp"
+import "strings"
 
-// Hey bob, answer me
+// remarkAnalyzer is used internally to expose various attributes
+type remarkAnalyzer struct {
+	remark string
+}
+
+// isYelling returns true if a remark is in all caps and alpha characters exist in the string
+func (r *remarkAnalyzer) isYelling() bool {
+	return strings.ToUpper(r.remark) == r.remark && strings.ToLower(r.remark) != r.remark
+}
+
+// isQuestion returns true if a remark ends with a question mark
+func (r *remarkAnalyzer) isQuestion() bool {
+	return strings.HasSuffix(r.remark, "?")
+}
+
+// isSilent returns true if the remark is empty
+func (r *remarkAnalyzer) isSilent() bool {
+	return r.remark == ""
+}
+
+// Hey will take a string and return the snarky response of a teenager named Bob
 func Hey(remark string) string {
-	toBeRemoved := regexp.MustCompile(`[\s!,]`)
-	subject := toBeRemoved.ReplaceAllLiteralString(remark, "")
-
-	isAQuestion := regexp.MustCompile(`\?$`)
-	isShouting := regexp.MustCompile(`^([^a-z])+$`)
-	isAnxious := regexp.MustCompile(`^[A-Z]+\?$`)
-	onlyNumbers := regexp.MustCompile(`^[0-9]+$`)
-
-	var answer string
-
+	analyzer := &remarkAnalyzer{strings.TrimSpace(remark)}
 	switch {
-	case onlyNumbers.MatchString(subject):
-		answer = "Whatever."
-	case isAnxious.MatchString(subject):
-		answer = "Calm down, I know what I'm doing!"
-	case isAQuestion.MatchString(subject):
-		answer = "Sure."
-	case isShouting.MatchString(subject):
-		answer = "Whoa, chill out!"
-	case subject == "":
-		answer = "Fine. Be that way!"
+	case analyzer.isQuestion() && analyzer.isYelling():
+		return "Calm down, I know what I'm doing!"
+	case analyzer.isQuestion():
+		return "Sure."
+	case analyzer.isYelling():
+		return "Whoa, chill out!"
+	case analyzer.isSilent():
+		return "Fine. Be that way!"
 	default:
-		answer = "Whatever."
+		return "Whatever."
 	}
-	return answer
 }
