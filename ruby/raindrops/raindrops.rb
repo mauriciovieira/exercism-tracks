8b276379520dfa73f7ea172c6025016b4e4cec67
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-27 18:59:39 +0300

    [:gem:] raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,19 @@
+module BookKeeping
+  VERSION = 3
+end
+
+class Raindrops
+  def self.convert(number)
+    answers = []
+
+    answers << "Pling" if (number % 3).zero?
+    answers << "Plang" if (number % 5).zero?
+    answers << "Plong" if (number % 7).zero?
+    
+    if answers.empty?
+      number.to_s
+    else
+      answers.join('')
+    end
+  end
+end
