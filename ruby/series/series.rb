9071725c1fde825c62d87fb7b5d83fef7084b243
Mauricio Vieira <mauricio@mauriciovieira.net>
2019-03-10 17:19:01 -0300

    [:gem:] series (rubistic)

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,15 +1,10 @@
 class Series
   def initialize(number)
-    @digits = number.split ''
+    @digits = number.chars
   end
 
   def slices(n)
     raise ArgumentError if n > @digits.size
-
-    slices = []
-    @digits.each_with_index do |e, i|
-      slices << @digits.slice(i, n).join if i+n <= @digits.size
-    end
-    slices
+    @digits.each_cons(n).map(&:join)
   end
 end
